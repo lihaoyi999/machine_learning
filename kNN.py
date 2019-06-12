0a028a6d66421e15ebd4cb663f7ef341ef1685b4
lihaoyi <lihaoyi3052@163.com>
2019-06-12 10:37:43 +0800

    更新knn文件

--- kNN.py
+++ kNN.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
-# author : 囧囧有神
+# author : lihaoyi2048
 # data : 2019/06/11 17:50
 
 
 import numpy as np
 import operator
+import os
 
 
 def createDataSet():
@@ -18,12 +19,13 @@ def classify0(inX, dataSet, labels, k):
     Parameters
     ----------
     inX: 输入向量
-    dataSet: 训练集
+    dataSet: 训练集矩阵
     labels: 标签向量，训练集的分类标签
     k：选择最近邻居的数目
     :return:
     """
     dataSetSize = dataSet.shape[0]
+    # np.tile 将变量复制成与训练集相同大小的矩阵
     diffMat = np.tile(inX, (dataSetSize, 1)) - dataSet
     sqDiffMat = diffMat**2
     sqDistances = sqDiffMat.sum(axis=1)
@@ -55,20 +57,95 @@ def file2matrix(filename):
     index = 0
     for line in arrayOLines:
         line = line.strip()
-        listFormLine = line.split('\t')
-        returnMat[index, :] = listFormLine[0:3]
-        classLabelVector.append(int(listFormLine[-1]))
+        listFromLine = line.split('\t')
+        returnMat[index, :] = listFromLine[0:3]
+        classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat, classLabelVector
 
 
 def autoNorm(dataSet):
-    minVals = dataSet.min(0)
-    maxVals = dataSet.max(0)
-    ranges = maxVals-minVals
-    normDataSet = np.zeros(shape(dataSet))
+    """
+    归一化数据集
+    :param
+    dataSet: 数据集
+    :return:
+    normDataSet: 返回归一化后的数据集
+    ranges: 每列的极差
+    minVals: 每列的最小值
+    """
+    minVals = dataSet.min(0)  # 每列的最小值
+    maxVals = dataSet.max(0)  # 每列的最大值
+    ranges = maxVals - minVals  # 每列的极差
+    normDataSet = np.zeros(np.shape(dataSet))
     m = dataSet.shape[0]
     normDataSet = dataSet - np.tile(minVals, (m, 1))
-    normDataSet = normDataSet/np.tile(ranges, (m, 1))
+    normDataSet = normDataSet / np.tile(ranges, (m, 1))
     return normDataSet, ranges, minVals
 
+
+def datingClassTest():
+    hoRatio = 0.10
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    m = normMat.shape[0]
+    numTestVecs = int(m * hoRatio)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :],
+                                     datingLabels[numTestVecs:m], 3)
+        print("the calssifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]):
+            errorCount += 1.0
+    print("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing video games?"))
+    ffMiles = float(input("frequent flier miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = np.array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr - minVals) / ranges, normMat, datingLabels, 3)
+    print("You will probably like this person: ", resultList[classifierResult - 1])
+
+
+def img2vector(filename):
+    returnVect = np.zeros((1, 1024))
+    fr = open(filename)
+    for i in range(32):
+        lineStr = fr.readline()
+        for j in range(32):
+            returnVect[0, 32 * i + j] = int(lineStr[j])
+    return returnVect
+
+# 手写数字识别
+
+
+def handwritingClassTest():
+    hwLabels = []
+    trainingFileList = os.listdir('trainingDigits')
+    m = len(trainingFileList)
+    trainingMat = np.zeros((m, 1024))
+    for i in range(m):
+        fileNameStr = trainingFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        hwLabels.append(classNumStr)
+        trainingMat[i, :] = img2vector('trainingDigits/%s' % fileNameStr)
+    testFileList = os.listdir('testDigits')
+    errorCount = 0.0
+    mTest = len(testFileList)
+    for i in range(mTest):
+        fileNameStr = testFileList[i]
+        fileStr = fileNameStr.split('.')[0]
+        classNumStr = int(fileStr.split('_')[0])
+        vectorUnderTest = img2vector('testDigits/%s' % fileNameStr)
+        classifierResult = classify0(vectorUnderTest, trainingMat, hwLabels, 3)
+        print("\nthe classifier came back with: %d, the real answer is: %d" % (classifierResult, classNumStr))
+        if (classifierResult != classNumStr):
+            errorCount += 1.0
+    print("\nthe total number of errors is: %d" % errorCount)
+    print("\nthe total error rate is: %f" % (errorCount / float(mTest)))
--- run.py
+++ run.py
@@ -2,19 +2,23 @@ import numpy as np
 import kNN
 
 
-group, labels = kNN.createDataSet()
-print(kNN.classify0([0, 0], group, labels, 3))
-
-datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
-print(datingDataMat)
-print(datingLabels)
-
-import matplotlib.pyplot as plt
-fig = plt.figure()
-ax = fig.add_subplot(111)
-ax.scatter(datingDataMat[:, 1],datingDataMat[:, 2])
-plt.show()
-
-ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2],
-           15.0 * np.array(datingLabels), 15 * np.array(datingLabels))
-plt.show()
+# group, labels = kNN.createDataSet()
+# print(kNN.classify0([0, 0], group, labels, 3))
+#
+# datingDataMat, datingLabels = kNN.file2matrix('datingTestSet2.txt')
+# print(datingDataMat)
+# print(datingLabels)
+#
+# import matplotlib.pyplot as plt
+# fig = plt.figure()
+# ax = fig.add_subplot(111)
+# ax.scatter(datingDataMat[:, 1],datingDataMat[:, 2])
+# plt.show()
+#
+# ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2],
+#            15.0 * np.array(datingLabels), 15 * np.array(datingLabels))
+# plt.show()
+#
+# kNN.datingClassTest()
+# kNN.classifyPerson()
+kNN.handwritingClassTest()
